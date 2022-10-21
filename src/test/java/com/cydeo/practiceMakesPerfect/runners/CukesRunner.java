b71df842a066c860de3fe887fcebbe2d51b808e8
Adnan Sen <senadnan82@gmail.com>
2022-10-21 11:00:15 +0300

    first commit

---
+++ src/test/java/com/cydeo/practiceMakesPerfect/runners/CukesRunner.java
@@ -0,0 +1,27 @@
+package com.cydeo.practiceMakesPerfect.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+
+        features = "src/test/resources/features",
+        glue = "com/cydeo/practiceMakesPerfect/step_definitions",
+        dryRun = false,
+        tags = ""
+
+)
+
+
+public class CukesRunner {
+
+
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,19 @@
+feature: Library app login feature
+User Story:
+As a user, I should be able to login with correct
+credentials to different accounts
+
+Background: Assuming user is on the login page
+Given user is on the login page
+
+@librarian @employee
+  Scenario: Login as librarian
+  When user enters librarian username
+  And user enters librarian password
+  Then user should see the dashboard
+
+@student
+  Scenario: Login as student
+  When user enters student username
+  And user enters student password
+  Then user should see the dashboard
