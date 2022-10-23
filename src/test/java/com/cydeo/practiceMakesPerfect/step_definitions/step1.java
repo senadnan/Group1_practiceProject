130e1023843f174ce3439646bc3ec5ff1e5f0bc6
Hanife Dogan <leylakbahcesi11@gmail.com>
2022-10-23 09:26:23 +0300

    hanife s change

--- src/test/java/com/cydeo/practiceMakesPerfect/step_definitions/step1.java
+++ src/test/java/com/cydeo/practiceMakesPerfect/step_definitions/step1.java
@@ -1,4 +1,10 @@
 package com.cydeo.practiceMakesPerfect.step_definitions;
 
 public class step1 {
+
+    public static void main(String[] args) {
+        System.out.println(" by hanife");
+    }
+
+
 }
